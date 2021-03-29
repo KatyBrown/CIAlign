9dada0b3855057a46378dc23bcf821e1fe370e92
KatyBrown <thekatybrown@gmail.com>
2021-03-29 15:00:14 +0100

    modified shell scripts for indelible

--- benchmarking/INDELible/run_alignment.sh
+++ benchmarking/INDELible/run_alignment.sh
@@ -1,24 +1,16 @@
-for n in nucleotide amino_acid codon;
+
+for tool in clustal muscle mafft;
 do
-    
-    for tool in clustal muscle mafft;
+    for i in {1..100};
     do
-	for i in {1..100};
-	do
-	    mkdir $n/sim\_$i/$tool
-	done
+	mkdir nucleotide/sim\_$i/$tool
     done
 done
 
-
-
 for i in {1..100};
 do
-    for n in nucleotide amino_acid codon;
-	     do
-		 clustalo --auto -i $n/sim\_$i/sequences.fasta -o $n/sim\_$i/clustal/auto.fasta
-		 mafft --localpair --maxiterate 100 $n/sim\_$i/sequences.fasta > $n/sim\_$i/mafft/local_max100.fasta
-		 mafft --maxiterate 100 $n/sim\_$i/sequences.fasta > $n/sim\_$i/mafft/global_max100.fasta
-		 muscle -maxiters 100 -in $n/sim\_$i/sequences.fasta -out $n/sim\_$i/muscle/max100.fasta
-    done
+    clustalo --auto -i nucleotide/sim\_$i/sequences.fasta -o nucleotide/sim\_$i/clustal/auto.fasta
+    mafft --localpair --maxiterate 100 nucleotide/sim\_$i/sequences.fasta > nucleotide/sim\_$i/mafft/local_max100.fasta
+    mafft --maxiterate 100 nucleotide/sim\_$i/sequences.fasta > nucleotide/sim\_$i/mafft/global_max100.fasta
+    muscle -maxiters 100 -in nucleotide/sim\_$i/sequences.fasta -out nucleotide/sim\_$i/muscle/max100.fasta
 done
--- benchmarking/INDELible/run_simulations.sh
+++ benchmarking/INDELible/run_simulations.sh
@@ -1,21 +1,21 @@
-for typ in nucleotide amino_acid codon;
+typ=nucleotide
+
+mkdir $typ
+cp control\_$typ.txt control.txt
+for j in {1..100};
 do
-    mkdir $typ
-    cp control\_$typ.txt control.txt
-    for j in {1..100};
+    mkdir $typ/sim\_$j
+    indelible
+    for i in {2..9};
     do
-	mkdir $typ/sim\_$j
-	indelible
-	for i in {2..9}; do
-	    awk -v i=$i '{if(NR % 11 == i && x == 0)
-                            {printf(">%s\n%s", $1, $2); x = x + 1}
-                          else if (NR % 11 == i)
-                            {printf($2)}}; END{printf("\n")}' currentOut_TRUE.phy;
-	done > $typ/sim\_$j/alignment_true.fasta
-    sed 's/-//g' $typ/sim\_$j/alignment\_true.fasta > $typ/sim\_$j/sequences.fasta
-    rm currentOut*
-    mv LOG.txt $typ/sim\_$j/log.txt
-    awk 'NR == 7' trees.txt  | cut -f9 > $typ/sim\_$j/tree.nwk
-    rm trees.txt
-    done
+	awk -v i=$i '{if(NR % 11 == i && x == 0)
+                         {printf(">%s\n%s", $1, $2); x = x + 1}
+                      else if (NR % 11 == i)
+                         {printf($2)}}; END{printf("\n")}' currentOut_TRUE.phy;
+    done > $typ/sim\_$j/alignment_true.fasta
+sed 's/-//g' $typ/sim\_$j/alignment\_true.fasta > $typ/sim\_$j/sequences.fasta
+rm currentOut*
+mv LOG.txt $typ/sim\_$j/log.txt
+awk 'NR == 7' trees.txt  | cut -f9 > $typ/sim\_$j/tree.nwk
+rm trees.txt
 done
