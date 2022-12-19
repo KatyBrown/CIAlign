0659a0df3b4fda558087f92065e8637bed2ecbee
KatyBrown <thekatybrown@gmail.com>
2022-12-19 19:24:00 +0000

    wrote tests for new functions

--- tests/utilityFunctionsTest.py
+++ tests/utilityFunctionsTest.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+import logging
 import unittest
 from unittest import mock
 from mock import patch
@@ -12,7 +12,7 @@ from Bio import AlignIO
 import os
 from os import path
 import sys
-
+import io
 import warnings
 
 import CIAlign
@@ -25,13 +25,19 @@ class UtilityFunctionsgetColoursTests(unittest.TestCase):
 
     def testGetAAColours(self):
         AAcolours = utilityFunctions.getAAColours()
-
+        self.assertEqual(len(AAcolours), 28)
+        AAcolours = utilityFunctions.getAAColours(palette='bright')
         self.assertEqual(len(AAcolours), 28)
 
     def testGetNtColours(self):
         Ntcolours = utilityFunctions.getNtColours()
-
         self.assertEqual(len(Ntcolours), 18)
+        Ntcolours = utilityFunctions.getNtColours(palette='bright')
+        self.assertEqual(len(Ntcolours), 18)
+
+    def testGetMarkupColours(self):
+        Mcolours = utilityFunctions.getMarkupColours()
+        self.assertEqual(len(Mcolours), 7)
 
 
 class UtilityFunctionsMSAInputTests(unittest.TestCase):
@@ -39,9 +45,11 @@ class UtilityFunctionsMSAInputTests(unittest.TestCase):
     def setUp(self):
         self.input = "./tests/test_files/example1.fasta"
         self.in_array, self.nams = readMSA(self.input)
+        self.rmfile = 'mock_rmfile.txt'
 
     def tearDown(self):
-        pass
+        if os.path.exists(self.rmfile):
+            os.remove(self.rmfile)
 
     def testReplaceUbyT(self):
         result_ali = utilityFunctions.replaceUbyT(self.in_array, rev=False)
@@ -52,6 +60,16 @@ class UtilityFunctionsMSAInputTests(unittest.TestCase):
         self.assertFalse(len(findU[1]) > 0)
         self.assertFalse(len(findu[0]) > 0)
         self.assertFalse(len(findu[1]) > 0)
+
+    def testReplaceTbyU(self):
+        result_ali = utilityFunctions.replaceUbyT(self.in_array, rev=True)
+        findT = np.where(result_ali == "T")
+        findt = np.where(result_ali == "t")
+
+        self.assertFalse(len(findT[0]) > 0)
+        self.assertFalse(len(findT[1]) > 0)
+        self.assertFalse(len(findt[0]) > 0)
+        self.assertFalse(len(findt[1]) > 0)
 
     def testUnAlign(self):
         result_ali = utilityFunctions.unAlign(self.in_array)
@@ -71,6 +89,52 @@ class UtilityFunctionsMSAInputTests(unittest.TestCase):
         self.assertEqual(len(nams), len(self.nams))
         self.assertTrue((ali == self.in_array).all())
         self.assertTrue(nams == self.nams)
+
+    @parameterized.expand([[[22, 23, 24, 25, 26, 27],
+                            list(np.arange(0, 98))]])
+    def testRemoveColumns(self, rmAbsolute, relativePositions):
+        logger = logging.getLogger('path.to.module.under.test')
+        with mock.patch.object(logger, 'debug') as mock_debug:
+            arr, nams = utilityFunctions.FastaToArray(self.input, logger)
+        newarr, newrel, newrm = utilityFunctions.removeColumns(
+            rmAbsolute, relativePositions, arr,
+            logger, self.rmfile,'func')
+        expected_arr, expected_nams = utilityFunctions.FastaToArray(
+            "tests/test_files/remove_insertions_cleaned.fasta")
+        self.assertTrue((newarr == expected_arr).all())
+        self.assertEqual(sorted(list(newrm)), [22, 23, 24, 25, 26, 27])
+
+
+class UtilityFunctionsMSAEmpty(unittest.TestCase):
+
+    def setUp(self):
+        self.input = "./tests/test_files/empty.fasta"
+
+    def tearDown(self):
+        pass
+
+    @parameterized.expand([
+            ['./tests/test_files/corrupt.fasta']])
+    def testFastaToArrayCorrupt(self, ali_path):
+        logger = logging.getLogger('path.to.module.under.test')
+
+        # capture output code from @paxdiablo on
+        # https://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print
+        capturedOutput = io.StringIO()
+        capturedError = io.StringIO()
+        sys.stdout = capturedOutput
+        sys.stderr = capturedError
+        with self.assertRaises(SystemExit) as cm:
+            ali, nams = utilityFunctions.FastaToArray(ali_path, logger)
+        self.assertEqual(cm.exception.code, 1)
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        printed_stdout = capturedOutput.getvalue()
+        printed_stderr = capturedError.getvalue()
+        
+        self.assertTrue('needs to be in FASTA format' in printed_stdout)
+        self.assertTrue('needs to be in FASTA format' in printed_stderr)
 
 
 class UltilityFunctionsWriteOutfileTest(unittest.TestCase):
@@ -101,10 +165,35 @@ class UtilityFunctionsCheckSeqTest(unittest.TestCase):
     ])
     def testSeqType(self, input, expected):
         in_array, names = readMSA(input)
-
-        type = utilityFunctions.seqType(in_array)
+        logger = logging.getLogger('path.to.module.under.test')
+        type = utilityFunctions.seqType(in_array, logger)
 
         self.assertEqual(type, expected)
+
+    @parameterized.expand([
+            ['./tests/test_files/nonIUPAC.fasta']])
+    def testSeqTypeNonIUPAC(self, input):
+        arr, nams = utilityFunctions.FastaToArray(input)
+        logger = logging.getLogger('path.to.module.under.test')
+
+        capturedOutput = io.StringIO()
+        capturedError = io.StringIO()
+        sys.stdout = capturedOutput
+        sys.stderr = capturedError
+        with self.assertRaises(SystemExit) as cm:
+            ali, nams = utilityFunctions.seqType(arr, logger)
+        self.assertEqual(cm.exception.code, 1)
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        printed_stdout = capturedOutput.getvalue()
+        printed_stderr = capturedError.getvalue()
+        
+        self.assertTrue('Unknown nucleotides or amino acids detected'
+                        in printed_stdout)
+        self.assertTrue('Unknown nucleotides or amino acids detected'
+                        in printed_stderr)
+
 
 class UtilityFunctionsListFontsTest(unittest.TestCase):
 
@@ -123,3 +212,163 @@ class UtilityFunctionsListFontsTest(unittest.TestCase):
             if plat != "win32" and plat != "cygwin":
                 utilityFunctions.listFonts(self.outfile)
                 self.assertTrue(os.path.isfile(self.outfile))
+
+class UtilityFunctionsUpdateNamsTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @parameterized.expand([
+            [['Hi', 'I', 'Am', 'A', 'Test'], set(['Am']),
+             ['Hi', 'I', 'A', 'Test']],
+            [['Hi', 'I', 'Am', 'A', 'Test'], set(['The']),
+             ['Hi', 'I', 'Am', 'A', 'Test']],
+            [['Hi', 'I', 'Am', 'A', 'Test'],
+             set(['Test', 'A', 'Am', 'I', 'Hi']),
+             []]
+            ])
+    def testUpdateNams(self, nams, removed, expected):
+        rm = utilityFunctions.updateNams(nams, removed)
+        self.assertEqual(rm, expected)
+
+
+class UtilityFunctionscheckArrLengthTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @parameterized.expand([[np.zeros([0, 0]), "removed all of the sequences"],
+                           [np.zeros([0, 4]), "removed all of the sequences"],
+                           [np.zeros([3]), "not all the same length"]])
+    def testCheckArrLength0(self, arr, expected):
+        logger = logging.getLogger('path.to.module.under.test')
+        # capture output code from @paxdiablo on
+        # https://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print
+        capturedOutput = io.StringIO()
+        capturedError = io.StringIO()
+        sys.stdout = capturedOutput
+        sys.stderr = capturedError
+        with self.assertRaises(SystemExit) as cm:
+            utilityFunctions.checkArrLength(arr, logger)
+        self.assertEqual(cm.exception.code, 1)
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        printed_stdout = capturedOutput.getvalue()
+        printed_stderr = capturedError.getvalue()
+        
+        self.assertTrue(expected in printed_stdout)
+        self.assertTrue(expected in printed_stderr)
+
+    @parameterized.expand([[np.zeros([10, 4])]])
+    def testCheckArrLengthNot0(self, arr):
+        logger = logging.getLogger('path.to.module.under.test')
+        # capture output code from @paxdiablo on
+        # https://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print
+        capturedOutput = io.StringIO()
+        capturedError = io.StringIO()
+        sys.stdout = capturedOutput
+        sys.stderr = capturedError
+
+        utilityFunctions.checkArrLength(arr, logger)
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        printed_stdout = capturedOutput.getvalue()
+        printed_stderr = capturedError.getvalue()
+        
+        self.assertTrue(len(printed_stdout) == 0)
+        self.assertTrue(len(printed_stderr) == 0)
+
+
+class UtilityFunctionsRetainTest(unittest.TestCase):
+
+    def setUp(self):
+        self.arr, self.nams = utilityFunctions.FastaToArray(
+            "tests/test_files/example1.fasta")
+
+    def tearDown(self):
+        pass
+
+    @parameterized.expand([[None, [""], "", np.array([])],
+                          [['Seq1'], [""], "", np.array(['Seq1'])],
+                          [[''], ["S"], "", np.array(['Seq1',
+                                                      'Seq2',
+                                                      'Seq3',
+                                                      'Seq4',
+                                                      'Seq5',
+                                                      'Seq6',
+                                                      'Seq7'])],
+                          [[''], [""], "tests/test_files/keep.txt",
+                           np.array(['Seq2',
+                                     'Seq3'])],
+                          [['Seq1'], ["7"], "tests/test_files/keep.txt",
+                           np.array(['Seq1', 'Seq2', 'Seq3', 'Seq7'])]])
+    def testConfigRetainSeqs(self, retain, retainS, retainL, expected):
+        logger = logging.getLogger('path.to.module.under.test')
+        keeps = utilityFunctions.configRetainSeqs(retain, retainS, retainL,
+                                                  self.nams, "func", logger,
+                                                  False)
+        self.assertTrue(np.array_equal(keeps, expected))
+
+    def testConfigRetainSeqsFNF(self):
+        logger = logging.getLogger('path.to.module.under.test')
+        
+        with self.assertRaises(FileNotFoundError):
+            utilityFunctions.configRetainSeqs(None, [""], "hello.txt",
+                                              self.nams, "func", logger,
+                                              False)
+
+    def testConfigRetainSeqsNoMatch(self):
+        logger = logging.getLogger('path.to.module.under.test')
+    
+        capturedOutput = io.StringIO()
+        capturedError = io.StringIO()
+        sys.stdout = capturedOutput
+        sys.stderr = capturedError
+
+        utilityFunctions.configRetainSeqs(None, ["hello"], "",
+                                          self.nams, "func", logger,
+                                          False)
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        printed_stdout = capturedOutput.getvalue()
+        printed_stderr = capturedError.getvalue()
+        
+        self.assertTrue("No sequence names matching" in printed_stdout)
+        self.assertTrue("No sequence names matching" in printed_stderr)
+
+    @parameterized.expand([[['S1'], [""], ""],
+                           [None, ["S"], "tests/test_files/keep_m.txt"]])
+    def testConfigRetainSeqsNoMatchList(self, retain, retainS, retainL):
+        logger = logging.getLogger('path.to.module.under.test')
+
+        with self.assertRaises(RuntimeError):
+            utilityFunctions.configRetainSeqs(retain, retainS, retainL,
+                                              self.nams, "func", logger,
+                                              False)
+
+
+class UtilityFunctionsUpdateStartEndTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @parameterized.expand([[0, 10, [5, 6, 7], [0, 7]],
+                          [10, 20, [], [10, 20]],
+                          [10, 20, [5, 6, 15], [8, 17]]])
+    def testConfigRetainSeqs(self, start, end, removed, expected):
+        newstart, newend = utilityFunctions.updateStartEnd(start, end, removed)
+        self.assertEqual(newstart, expected[0])
+        self.assertEqual(newend, expected[1])
+        
