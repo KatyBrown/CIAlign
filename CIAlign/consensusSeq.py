45c693b0ac309709bf903b32271357d507d6154f
KatyBrown <thekatybrown@gmail.com>
2022-12-20 18:16:41 +0000

    rotated labels on stats plots

--- CIAlign/consensusSeq.py
+++ CIAlign/consensusSeq.py
@@ -296,7 +296,8 @@ def makeLinePlot(stat, dest, ylab, dpi=300, height=3, width=5,
     # c.plot(xx, p(xx))
     for sp in f.axes:
         sp.set_xticks(np.arange(0, xmax*1.1, x_div))
-        sp.set_xticklabels(np.arange(0, xmax*1.1, x_div), fontsize=fontsize)
+        sp.set_xticklabels(np.arange(0, xmax*1.1, x_div), fontsize=fontsize,
+                           rotation='vertical')
         sp.set_yticks(np.arange(0, ymax*1.1, y_div))
         sp.set_yticklabels([
             "%.1f" % y for y in np.arange(0, ymax*1.1, y_div)],
